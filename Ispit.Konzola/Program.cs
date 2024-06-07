68070aaa01460591d507892705220b18d4a8cf98
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-06-07 18:38:19 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,40 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Ucenik> list = new List<Ucenik>();
+            while (list.Count < 3)
+            {
+                Ucenik u = new Ucenik();
+                Console.WriteLine($"Unesite podatke za {list.Count + 1}. učenika");
+
+                Console.WriteLine("Ime: ");
+                u.Ime = Console.ReadLine();
+
+                Console.WriteLine("Prezime: ");
+                u.Prezime = Console.ReadLine();
+
+                Console.Write("Datum rođenja (dd/MM/yyyy): ");
+                u.DatumRodjenja = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Prosjek: ");
+                u.Prosjek = Convert.ToDouble(Console.ReadLine());
+                
+                list.Add(u);
+            }
+            Console.Clear();
+            foreach (Ucenik u in list)
+            {
+                u.UcenikInfo();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public double Prosjek { get; set; }
+
+        public Ucenik() { }
+        public string Starost()
+        {
+            TimeSpan TS = DateTime.Today - DatumRodjenja;
+            int x = (int)TS.TotalMinutes;
+
+            int dani = (int)x / 1440;
+            int mjeseci = dani / 30;
+            int ostatakMjeseca = mjeseci % 12;
+            int godineukupno = mjeseci / 12;
+            return $"{godineukupno} godina i {ostatakMjeseca} mjeseci";
+        }
+
+        public void IspisProsjeka()
+        {
+            if(Prosjek > 1 && Prosjek <= 1.49)
+            {
+                Console.WriteLine("Nedovoljan - {0}", Prosjek);
+            }
+            else if(Prosjek >= 1.50 && Prosjek <= 2.49)
+            {
+                Console.WriteLine($"Dovoljan - {Prosjek}");
+            }
+            else if (Prosjek >= 2.50 &&  Prosjek <= 3.49)
+            {
+                Console.WriteLine($"Dobar - {Prosjek}");
+            }
+            else if (Prosjek >= 3.50 && Prosjek <= 4.49)
+            {
+                Console.WriteLine($"Vrlo dobar - {Prosjek}");
+            }
+            else if (Prosjek >= 4.50 && Prosjek <= 5.00)
+            {
+                 Console.WriteLine($"Odličan - {Prosjek}");             
+            }
+            else
+            {
+                Console.WriteLine($"Nepravilan prosjek - {Prosjek}");
+            }
+        }
+
+        public void UcenikInfo()
+        {
+            Console.WriteLine($"{Ime} {Prezime}");
+            IspisProsjeka();
+            Console.WriteLine(Starost());
+        }
+    }
+}
